e83986af01bd8ab2291cc30f722925c5ab7a2827
Soninja <145279686+s0ninja@users.noreply.github.com>
2024-09-10 16:56:53 +0200

    Add files via upload

---
+++ Soundboard Website/Soundboard Website/execute.js
@@ -0,0 +1,20 @@
+const soundButton1 = document.getElementById('playSound1');
+const soundEffect1 = document.getElementById('soundEffect1');
+
+soundButton1.addEventListener('click', () => {
+    soundEffect1.play();
+});
+
+const soundButton2 = document.getElementById('playSound2');
+const soundEffect2 = document.getElementById('soundEffect2');
+
+soundButton2.addEventListener('click', () => {
+    soundEffect2.play();
+});
+
+const soundButton3 = document.getElementById('playSound2');
+const soundEffect3 = document.getElementById('soundEffect2');
+
+soundButton3.addEventListener('click', () => {
+    soundEffect3.play();
+});
